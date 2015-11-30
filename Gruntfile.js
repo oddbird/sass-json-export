55428b8ffeb69f727ae57dc875fdb4c895595345
Jonny Gerig Meyer <jonny@oddbird.net>
2015-11-30 13:04:19 -0500

    Upgrade deps; remove Ruby.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,10 @@
 module.exports = function(grunt) {
 
   // Modules
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-scsslint');
   grunt.loadNpmTasks('bootcamp');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-sass-lint');
 
   // Grunt Tasks
   grunt.initConfig({
@@ -16,37 +16,19 @@ module.exports = function(grunt) {
     // Sass
     sass: {
       options: {
-        style: 'expanded',
-        loadPath: ['./node_modules/bootcamp/dist', './<%= dir.src %>']
+        outputStyle: 'expanded',
+        includePaths: ['./node_modules/bootcamp/dist', './<%= dir.src %>']
       },
       all: {
         files: {
           './tmp/all.css': './test/test.scss'
         }
-      },
-      encoder: {
-        files: {
-          './tmp/encode.css': './test/encode/test.scss'
-        }
       }
     },
 
-    scsslint : {
-      options : {
-        excludeLinter : [
-          'ColorKeyword',
-          'NameFormat',
-          'StringQuotes',
-          'SpaceAfterComma',
-          'Comment',
-          'PropertySpelling'
-        ]
-      },
+    sasslint : {
       all: {
         src : ['<%= dir.src %>/**/*']
-      },
-      encoder: {
-        src : ['<%= dir.src %>/encoder/**/*']
       }
     },
 
@@ -55,11 +37,6 @@ module.exports = function(grunt) {
       all: {
         files: {
           src: ['./tmp/all.css']
-        }
-      },
-      encoder:{
-        files: {
-          src: ['./tmp/encode.css']
         }
       }
     },
@@ -72,36 +49,11 @@ module.exports = function(grunt) {
                 './<%= dir.src %>/**/*.scss'
                 ],
         tasks: ['test']
-      },
-      encoder: {
-        files: [
-                './test/encode/**/*.scss',
-                './<%= dir.src %>/**/*.scss'
-                ],
-        tasks: ['test:encode']
       }
     }
   });
 
   // Tasks
-  grunt.registerTask('test', function (arg){
-    arg = arg || "all";
-
-    var tasks = ['scsslint', 'sass', 'bootcamp'].map(function (item){
-      return item + ":" + arg;
-    });
-
-
-    grunt.task.run(tasks);
-  });
-  grunt.registerTask('dev', function (arg ){
-    arg = arg || "all";
-
-    var tasks = ['test', 'watch'].map(function (item){
-      return item + ":" + arg;
-    });
-
-    grunt.task.run(tasks);
-  });
-  grunt.registerTask('build', ['test:all', 'concat']);
+  grunt.registerTask('test', ['sasslint', 'sass', 'bootcamp']);
+  grunt.registerTask('dev', ['test', 'watch']);
 };
